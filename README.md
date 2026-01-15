d17758a9138354ac1d5350822c090d3128ec378a
Deep Ghosh <deepghosh3141@gmail.com>
2026-01-15 20:22:13 +0530

    update

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# soundp
+# Streamy
+
+Streamy is a web music app built using Next.js 14, App Router, and an unofficial music API. The user interface is styled with Tailwind CSS.
+
+![Homepage](/public/feed.png)
+
+## Features
+
+- Browse and listen to a wide variety of music.
+- Light and Dark mode for user preference.
+- Search for your favorite artists, albums, and tracks.
+- Enjoy a seamless music listening experience.
+
+## Screenshots
+
+### Homepage
+
+![Homepage](/public/feed.png)
+
+### Search Page
+
+![Search Page](/public/search-feed.png)
+
+### Album Page
+
+![Album Page](/public/album.png)
+
+### Music Page
+
+Player
+
+![Music Page](/public/player-1.png)
+
+Mobile Player
+
+![Music Page](/public/player-2.png)
+
+## Installation
+
+1. Clone the repository:
+
+```bash
+git clone <your-repository-url>
+cd streamy
+```
+
+2. Install dependencies:
+
+```bash
+npm install or pnpm install
+```
+
+3. Run the development server:
+
+```bash
+npm run dev or pnpm run dev
+```
+
+Visit [http://localhost:3000](http://localhost:3000) in your browser to explore Streamy.
+
+## Setup Api
+
+fork and deploy your own repo of `https://github.com/sumitkolhe/jiosaavn-api` get the deployment url and paste in .env file refer .env.example
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ app/layout.js
@@ -0,0 +1,52 @@
+import MusicProvider from "@/components/providers/music-provider";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Bricolage_Grotesque } from "next/font/google";
+import NextTopLoader from "nextjs-toploader";
+import { Toaster } from "sonner";
+import "./globals.css";
+
+const bricolage_grotesque = Bricolage_Grotesque({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
+
+export const metadata = {
+  title: "Streamy",
+  description: "Music streaming app.",
+  icons: "/favi-icon.jpg",
+  manifest: "/manifest.json",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={bricolage_grotesque.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <NextTopLoader
+            color="hsl(var(--primary))"
+            initialPosition={0.08}
+            crawlSpeed={200}
+            height={3}
+            crawl={true}
+            showSpinner={false}
+            easing="ease"
+            speed={200}
+            shadow="0 0 10px hsl(var(--primary)),0 0 15px hsl(var(--primary))"
+            template='<div class="bar" role="bar"><div class="peg"></div></div>
+        <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+            zIndex={1600}
+            showAtBottom={false}
+          />
+          <MusicProvider>{children}</MusicProvider>
+          {/* <MobileMenu/> */}
+          <Toaster position="top-center" visibleToasts={1} />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
