0fc611e877643d7a0e2e3d0c9f2ab96497fe050f
Deep Ghosh <deepghosh3141@gmail.com>
2026-01-16 19:45:40 +0530

    Update app icons and metadata, add icon generation script
    Replaced and added various favicon, Apple touch, Android, iOS, and Windows icons for improved cross-platform support. Updated app metadata to use new icons. Added a script (generate-icons.mjs) and the 'sharp' dependency for automated icon generation. Removed old favicon and updated package scripts accordingly.

--- app/layout.js
+++ app/layout.js
@@ -14,8 +14,20 @@ const bricolage_grotesque = Bricolage_Grotesque({
 export const metadata = {
   title: APP_NAME,
   description: "Music streaming app.",
-  icons: "/favi-icon.jpg",
   manifest: "/manifest.json",
+  icons: {
+    icon: [
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: [
+      {
+        url: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
